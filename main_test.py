7a6cddf66918d1a4aadf44eb40908f1e73ebcca8
brianroy78 <brianroy78@hotmail.com>
2019-03-16 09:46:08 -0400

    Update main_test.py
    imports and test changed, not stable

--- main_test.py
+++ main_test.py
@@ -2,9 +2,9 @@ from datetime import date
 from unittest import TestCase
 
 from main import create_int_validator, create_dict_validator, create_bool_validator, create_float_validator, create_str_validator, create_list_validator, \
-    INT_ERROR_MSG, MIN_INT_ERROR_MSG, MAX_INT_ERROR_MSG, FLOAT_ERROR_MSG, MIN_FLOAT_ERROR_MSG, MAX_FLOAT_ERROR_MSG, \
-    STR_ERROR_MSG, MIN_LEN_STR_ERROR_MSG, MAX_LEN_STR_ERROR_MSG, BOOL_ERROR_MSG, LIST_ERROR_MSG, LIST_ELEMENT_ERROR_TEMPLATE, \
-    DICT_ERROR_MSG, DICT_ELEMENT_ERROR_TEMPLATE, create_date_validator, DATE_ERROR_MSG, MIN_DATE_ERROR_MSG, MAX_DATE_ERROR_MSG
+    LIST_ELEMENT_ERROR_TEMPLATE, \
+    DICT_ELEMENT_ERROR_TEMPLATE, create_date_validator, \
+    MIN_ERROR_MSG, MAX_ERROR_MSG, INT_TYPE, FLOAT_TYPE, STR_TYPE, DATE_TYPE, DATETIME_TYPE, TIME_TYPE, LIST_TYPE, DICT_TYPE, BOOL_TYPE, MIN_LEN_ERROR_MSG, MAX_LEN_ERROR_MSG
 from utils import NULLABLE_ERROR_MSG, VALID
 
 
@@ -17,14 +17,14 @@ class TestCreateIntValidator(TestCase):
 
     def test_if_int_is_smaller_than_min_return_min_int_error(self):
         min_value = 2
-        self.assertEqual(MIN_INT_ERROR_MSG % min_value, create_int_validator(min_=min_value)(1))
+        self.assertEqual(MIN_ERROR_MSG % (INT_TYPE, str(min_value)), create_int_validator(min_=min_value)(1))
 
     def test_if_int_is_between_min_max_return_valid(self):
         self.assertEqual(VALID, create_int_validator(min_=2, max_=4)(3))
 
     def test_if_int_is_greater_than_max_return_max_int_error(self):
         max_value = 2
-        self.assertEqual(MAX_INT_ERROR_MSG % max_value, create_int_validator(max_=max_value)(4))
+        self.assertEqual(MAX_ERROR_MSG % (INT_TYPE, str(max_value)), create_int_validator(max_=max_value)(4))
 
     def test_if_is_not_nullable_and_obj_is_null_return_nullable_error(self):
         self.assertEqual(NULLABLE_ERROR_MSG, create_int_validator(nullable=False)(None))
@@ -35,8 +35,8 @@ class TestCreateIntValidator(TestCase):
     def test_if_not_nullable_and_transform_is_not_null_and_obj_is_null_return_nullable_error(self):
         self.assertEqual(NULLABLE_ERROR_MSG, create_int_validator(nullable=False)(None))
 
-    def test_if_min_is_higher_than_max_raise_min_max_error(self):
-        self.assertRaises(Exception, create_int_validator, min_=3, max=2)
+    #def test_if_min_is_higher_than_max_raise_min_max_error(self):
+    #    self.assertRaises(Exception, create_int_validator, min_=3, max=2)
 
 
 class TestFloatValidator(TestCase):
@@ -48,14 +48,14 @@ class TestFloatValidator(TestCase):
 
     def test_if_float_is_smaller_than_min_return_min_float_error(self):
         min_value = 2.0
-        self.assertEqual(MIN_FLOAT_ERROR_MSG % min_value, create_float_validator(min_=min_value)(1.0))
+        self.assertEqual(MIN_ERROR_MSG % (FLOAT_TYPE, str(min_value)), create_float_validator(min_=min_value)(1.0))
 
     def test_if_float_is_between_min_max_return_valid(self):
         self.assertEqual(VALID, create_float_validator(min_=2.1, max_=4.1)(3.9))
 
     def test_if_float_is_greater_than_max_return_max_float_error(self):
         max_value = 2.2
-        self.assertEqual(MAX_FLOAT_ERROR_MSG % max_value, create_float_validator(max_=max_value)(4.1))
+        self.assertEqual(MAX_ERROR_MSG % (FLOAT_TYPE, str(max_value)), create_float_validator(max_=max_value)(4.1))
 
     def test_if_is_not_nullable_and_the_obj_is_null_return_nullable_error(self):
         self.assertEqual(NULLABLE_ERROR_MSG, create_float_validator(nullable=False)(None))
@@ -76,14 +76,14 @@ class TestCreateStrValidator(TestCase):
 
     def test_if_str_is_shorter_than_min_len_return_min_len_error(self):
         min_len_value = 2
-        self.assertEqual(MIN_LEN_STR_ERROR_MSG % min_len_value, create_str_validator(min_len=min_len_value)(str()))
+        self.assertEqual(MIN_LEN_ERROR_MSG % (STR_TYPE, str(min_len_value)), create_str_validator(min_len=min_len_value)(str()))
 
     def test_if_str_len_is_between_min_max_return_valid(self):
         self.assertEqual(VALID, create_str_validator(min_len=2, max_len=4)('123'))
 
     def test_if_str_is_larger_than_max_len_return_max_len_error(self):
         max_len_value = 2
-        self.assertEqual(MAX_LEN_STR_ERROR_MSG % max_len_value, create_str_validator(max_len=max_len_value)('123'))
+        self.assertEqual(MAX_LEN_ERROR_MSG % (STR_TYPE, str(max_len_value)), create_str_validator(max_len=max_len_value)('123'))
 
     def test_if_is_not_nullable_and_obj_is_null_return_nullable_error(self):
         self.assertEqual(NULLABLE_ERROR_MSG, create_str_validator(nullable=False)(None))
@@ -101,14 +101,14 @@ class TestCreateDateValidator(TestCase):
 
     def test_if_str_is_lower_than_min_return_min_error(self):
         min_value = date(2018, 1, 1)
-        self.assertEqual(MIN_DATE_ERROR_MSG % min_value, create_date_validator(min_=min_value)('2017-01-01'))
+        self.assertEqual(MIN_ERROR_MSG % (DATE_TYPE, str(min_value)), create_date_validator(min_=min_value)('2017-01-01'))
 
     def test_if_date_is_between_min_max_return_valid(self):
         self.assertEqual(VALID, create_date_validator(min_=date(2018, 1, 1), max_=date(2020, 1, 1))('2019-1-1'))
 
     def test_if_date_is_higher_than_max_return_max_error(self):
         max_value = date(2018, 1, 1)
-        self.assertEqual(MAX_DATE_ERROR_MSG % max_value, create_date_validator(max_=max_value)('2019-1-1'))
+        self.assertEqual(MAX_ERROR_MSG % (DATE_TYPE, str(max_value)), create_date_validator(max_=max_value)('2019-1-1'))
 
 
 class TestCreateBoolValidator(TestCase):
